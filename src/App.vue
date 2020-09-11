a49036dbccf09e0061ba789786970f7a8d7d95c4
Michael Saylor <msaylor@pureromance.com>
2020-09-11 19:43:18 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+
+export default {
+  components: {
+    appQuoteGrid: QuoteGrid},
+  data() {
+          return {
+            quotes: ['Just a Quote to see something'],
+            maxQuotes: 10
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
